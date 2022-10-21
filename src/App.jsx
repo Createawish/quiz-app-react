3aadda4fe0c2ccf95e327ab5d50a7fb7d5cf282d
Createawish <bezlyudovalera1996@gmail.com>
2022-10-21 15:09:56 +0300

    Feature: add background img and create layout

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import React from "react";
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='main'>g</div>
+      <div className='pyramid'>g</div>
+    </div>
+  );
+}
+
+export default App;
