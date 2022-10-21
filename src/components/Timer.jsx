e25534f5b38287772e5c1dcb3d5906e2c4d563dc
Createawish <bezlyudovalera1996@gmail.com>
2022-10-22 01:24:10 +0300

    Feature: add sound in the start

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,15 +1,19 @@
 import React, {useEffect, useState} from 'react';
 
-const Timer = () => {
+const Timer = ({setStop,questionNumber}) => {
     const [timer,setTimer] = useState(30)
 
 
     useEffect(()=>{
-        const interval = setInterval(()=>{}, 1000)
-        setTimer(prev=> prev-1)
-    },[])
+        if(timer === 0) return setStop(true);
+        const interval = setInterval(()=>{setTimer(prev=> prev-1)}, 1000);
+        return ()=> clearInterval(interval)
+    },[setStop,timer])
 
 
+    useEffect(()=>{
+    setTimer(30)
+    },[questionNumber])
     return timer;
 };
 
--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,11 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import '../App.css';
+import useSound from 'use-sound';
+import play from '../assets/src_sounds_play.mp3'
+import correct from '../assets/src_sounds_correct.mp3';
+import wait from '../assets/src_sounds_wait.mp3';
+import error from '../assets/src_sounds_wrong.mp3';
 
 const Trivia = ({data,setStop,setQuestionNumber,questionNumber}) => {
 
     const [question, setQuestion]= useState(null);
     const [selectedAnswer, setSelectedAnswer] = useState(null);
     const [className, setClassName] = useState('answer')
+    const [letsPlay] = useSound(play);
+    const [correctAnswer] = useSound(correct);
+    const [letsWait] = useSound(wait);
+    const [errorAnswer] = useSound(error);
+
+    useEffect(()=>{
+        letsPlay()
+    },[letsPlay])
+
 
     const delay = (duration, callback) => {
 setTimeout(()=>{
@@ -14,15 +28,20 @@ setTimeout(()=>{
     };
 
     const handleClick = (a) => {
-        setSelectedAnswer(a);
+        setSelectedAnswer (a);
         setClassName('answer active');
         delay(3000, ()=>setClassName(a.correct ? 'answer correct' : 'answer error'))
        delay(6000, ()=>{
            if(a.correct){
-               setQuestionNumber(prev => prev+1);
-               setSelectedAnswer(null)
+               correctAnswer()
+               delay(1000,() => {
+                   setQuestionNumber(prev => prev + 1);
+                   setSelectedAnswer(null);
+               });
            }else {
-               setStop(true)
+               delay(1000,()=> {
+                   setStop(true)
+               })
            }
        })
     }
