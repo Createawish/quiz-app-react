7c05d54e127d681c63ff87e9c5f8e2d586a61861
Createawish <bezlyudovalera1996@gmail.com>
2022-10-21 15:31:34 +0300

    Feture: add arr pyramid-money

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,42 @@
 import './App.css';
 import React from "react";
 
+
+
+const moneyPyramid = [
+    {id:1, amount: '$ 100' },
+    {id:2, amount: '$ 200' },
+    {id:3, amount: '$ 300' },
+    {id:4, amount: '$ 500' },
+    {id:5, amount: '$ 1000' },
+    {id:6, amount: '$ 2000' },
+    {id:7, amount: '$ 4000' },
+    {id:8, amount: '$ 8000' },
+    {id:9, amount: '$ 16000' },
+    {id:10, amount: '$ 32000' },
+    {id:11, amount: '$ 64000' },
+    {id:12, amount: '$ 125000' },
+    {id:13, amount: '$ 250000' },
+    {id:14, amount: '$ 500000' },
+    {id:15, amount: '$ 1000000' },
+]
+
+
 function App() {
   return (
     <div className='app'>
       <div className='main'>g</div>
-      <div className='pyramid'>g</div>
+      <div className='pyramid'>
+          <ul className='moneyList'>
+              {moneyPyramid.map(m=>(
+                  <li className='moneyListItem active'>
+                      <span className='moneyItemNumber'>{m.id}</span>
+                      <span className='moneyListAmount'> {m.amount} </span>
+                  </li>
+              ))}
+
+          </ul>
+      </div>
     </div>
   );
 }
